b8eff96eb51239aa363e6673bc81cf3b44ef568f
Mario Rodríguez <mrodz07@proton.me>
2022-05-29 08:15:58 -0500

    Added error messages to login and changed it to .php

--- app/login.php
+++ app/login.php
@@ -2,10 +2,16 @@
   require_once "../models/UserManager.php";
 
   session_start();
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $error = "Usuario o contraseña incorrectos";
   $uM = UserManager::getInstance();
-  if ($uM->checkCredentials($_POST['username'], $_POST['password'])) {
+
+  if ($uM->checkCredentials($username, $password)) {
+      $_SESSION['username'] = $username;
       header('Location: list.php');
   } else {
+      $_SESSION['error'] = $error;
       header('Location: /');
   }
 ?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+  session_start();
+  if (isset($_SESSION['username']))
+  {
+    // User is not logged in, so send user away.
+    header("Location: /app/list.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Acceso al sistema</title>
+    <link rel="stylesheet" href="app/style.css">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+  </head>
+  <body>
+    <?php
+      if (isset($_SESSION["error"])) {
+        $error = $_SESSION["error"];
+        echo "<div class='info alert'>$error</div>";
+      }
+    ?>
+    <h2 class="main-title">Acceso al sistema</h2>
+    <div class="container">
+      <form action="/app/login.php" method="post">
+        <div class="form-group">
+          <label for="username">Correo:</label>
+          <input id="username" type="text" name="username" />
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña:</label>
+          <input id="password" type="password" name="password" />
+        </div>
+        <p>
+          <input class="button info" type="submit" name="login" value="Login" />
+        </p>
+      </form>
+    </div>
+  </body>
+</html>
+
+<?php
+  unset($_SESSION["error"]);
+?>
