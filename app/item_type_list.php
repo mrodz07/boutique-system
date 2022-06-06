aafdf57a37d54db5a5214e6f0f43c739f046983e
Mario Rodríguez <mrodz07@proton.me>
2022-06-06 13:01:17 -0500

    Deleted CarManager and added item_option_list.php

---
+++ app/item_type_list.php
@@ -0,0 +1,92 @@
+<?php
+  require_once "../models/UserManager.php";
+  session_start();
+  $userManager = UserManager::getInstance();
+  $username = $_SESSION['username'];
+
+  if (!isset($username)) {
+    $error = "Entra a tu cuenta antes de acceder al sistema";
+    // User is not logged in, so send user away.
+    $_SESSION['error'] = $error;
+    header("Location: /");
+    exit;
+  }
+
+  if(!$userManager -> isAdmin($username)) {
+    $error = "No estás autorizado para entrar a esta página";
+    // User is not logged in, so send user away.
+    $_SESSION['error'] = $error;
+    header("Location: /");
+    exit;
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Listado de marcas</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+  </head>
+  <body>
+    <?php
+      if (isset($_SESSION["error"])) {
+        $error = $_SESSION["error"];
+        echo "<div class='alert'>$error</div>";
+      }
+      if (isset($_SESSION["message"])) {
+        $message = $_SESSION["message"];
+        echo "<div class='info'>$message</div>";
+      }
+    ?>
+    <?php
+      require_once "../models/ItemManager.php";
+      $itemManager = ItemManager::getInstance();
+
+      $brands = $itemManager->getAllBrands();        
+      echo "<div class='user-menu-container'> <div class='greeting'>Bienvenido $username</div> <div class='user-menu'> <a class='button menu' href='/app/sale_list.php'>Ventas</a> <a class='button menu' href='/app/inventory_list.php'>Inventario</a> <a class='button menu' href='/app/item_option_list.php'>Artículos</a> <a class='button menu' href='/app/user_list.php'>Usuarios</a> <a class='button menu' href='/app/general_stats.php'>Estadísticas</a> <a class='button menu' href='/app/user_close.php'>Cerrar sesión</a>  </div> </div>";
+    ?>
+    <div class="container-full">
+      <h2 class="main-title">Lista de marcas</h2>
+          <div class="button-container">
+            <a class="button info" href="/app/brand_add.php">Añadir marca</a>
+          </div>
+          <?php if(!empty($brands)) { ?>
+          <div>
+            <table class="table">
+              <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Opciones</th>
+              </tr>
+              
+              <?php foreach($brands as $brand) { ?>
+                <tr>
+                  <td><?php echo $brand->id ?></td>
+                  <td><?php echo $brand->nombre ?></td>
+                  <td>
+                      <a class="button info" href="/app/brand_details.php?id=<?php echo $brand->id ?>">Detalles</a> 
+                      <a class="button info" href="/app/brand_edit.php?id=<?php echo $brand->id ?>">Editar</a> 
+                      <a class="button info" href="/app/brand_delete.php?id=<?php echo $brand->id ?>">Borrar</a>
+                  </td>
+                </tr>
+              <?php } ?>
+            </table>
+          </div>
+          <?php
+            } else {
+          ?>
+            <div class="msg alert">Hay 0 marcas registradas</div>
+          <?php
+            }
+          ?>
+      </div>
+    </div>
+  </body>
+</html>
+
+<?php
+  unset($_SESSION["error"]);
+  unset($_SESSION["message"]);
+?>
