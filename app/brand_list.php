958992e13f6d9eaac0d57e839f90a727ac144358
Mario Rodríguez <mrodz07@proton.me>
2022-06-06 12:22:59 -0500

    Fixed item_list.php bug

--- app/brand_list.php
+++ app/brand_list.php
@@ -2,6 +2,7 @@
   require_once "../models/UserManager.php";
   session_start();
   $userManager = UserManager::getInstance();
+  $username = $_SESSION['username'];
 
   session_start();
   if (!isset($_SESSION['username'])) {
@@ -47,12 +48,7 @@
       $userManager = UserManager::getInstance();
 
       $items = $itemManager->getAllSpec();        
-      $username = $_SESSION['username'];
-      if ($userManager -> isAdmin($username)) {
-        echo "<div class='user-menu-container'> <div class='greeting'>Bienvenido $username</div> <div class='user-menu'> <a class='button menu' href='/app/sale_list.php'>Ventas</a> <a class='button menu' href='/app/inventory_list.php'>Inventario</a> <a class='button menu' href='/app/item_option_list.php'>Artículos</a> <a class='button menu' href='/app/user_list.php'>Usuarios</a> <a class='button menu' href='/app/general_stats.php'>Estadísticas</a> <a class='button menu' href='/app/user_close.php'>Cerrar sesión</a>  </div> </div>";
-      } else {
-        echo "<div class='user-menu-container'> <div class='greeting'>Bienvenido $username</div> <div class='user-menu'> <a class='button menu' href='/app/sale_list.php'>Ventas</a> <a class='button menu' href='/app/user_edit.php?username=$username'>Contraseña</a><a class='button menu' href='/app/user_close.php'>Cerrar sesión</a> </div> </div>";
-      }
+      echo "<div class='user-menu-container'> <div class='greeting'>Bienvenido $username</div> <div class='user-menu'> <a class='button menu' href='/app/sale_list.php'>Ventas</a> <a class='button menu' href='/app/inventory_list.php'>Inventario</a> <a class='button menu' href='/app/item_option_list.php'>Artículos</a> <a class='button menu' href='/app/user_list.php'>Usuarios</a> <a class='button menu' href='/app/general_stats.php'>Estadísticas</a> <a class='button menu' href='/app/user_close.php'>Cerrar sesión</a>  </div> </div>";
     ?>
     <div class="container-full">
       <h2 class="main-title">Lista de artículos</h2>
